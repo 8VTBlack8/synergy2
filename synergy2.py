9c105a3b692f678d1f48658e121655505eb7e63a
8VTBlack8 <Vtblackvt@gmail.com>
2025-03-29 16:13:12 +0300

    Добавьте файлы проекта.

---
+++ synergy2.py
@@ -0,0 +1,6 @@
+stage1 = input("Введите первую стадию развития человека: ")  # Например, Australopithecus
+stage2 = input("Введите вторую стадию развития человека: ") # Например, Homo habilis
+stage3 = input("Введите третью стадию развития человека: ")  # Например, Homo erectus
+stage4 = input("Введите четвертую стадию развития человека: ") # Например, Homo neanderthalensis
+stage5 = "Homo sapiens sapiens"  # Последняя стадия
+print(stage1, stage2, stage3, stage4, stage5, sep=' => ') #Выводим результаты, разделенные символом =>
